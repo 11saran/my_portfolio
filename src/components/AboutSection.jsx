daa5155d83a12a3801e6b66b2238e84c43355c79
11saran <1211saran@gmail.com>
2025-05-03 21:57:33 +0530

    Initialize project structure with React, Vite, and Tailwind CSS

---
+++ src/components/AboutSection.jsx
@@ -0,0 +1,96 @@
+import { Briefcase, Code, Phone } from "lucide-react";
+import React from "react";
+
+const AboutSection = () => {
+  return (
+    <section id="about" className="py-24 px-4 relative">
+      {" "}
+      <div className="container mx-auto max-w-5xl">
+        <h2 className="text-3xl md:text-4xl font-bold mb-12 text-center">
+          About <span className="text-primary"> Me</span>
+        </h2>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
+          <div className="space-y-6">
+            <h3 className="text-2xl font-semibold">
+              Creative Full-Stack Developer & Mobile App Builder
+            </h3>
+
+            <p className="text-muted-foreground">
+              With 01 year of experience in web and mobile development, I build
+              responsive and efficient applications that offer seamless user
+              experiences across platforms.
+            </p>
+
+            <p className="text-muted-foreground">
+              I'm passionate about creating innovative solutions for complex
+              problems, and I continually explore new technologies in web and
+              mobile development to stay ahead of the curve.
+            </p>
+
+            <div className="flex flex-col sm:flex-row gap-4 pt-4 justify-center">
+              <a href="#contact" className="cosmic-button">
+                Get In Touch
+              </a>
+
+              <a
+                href=""
+                className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
+              >
+                Download CV
+              </a>
+            </div>
+          </div>
+
+          <div className="grid grid-cols-1 gap-6">
+            <div className="gradient-border p-6 card-hover">
+              <div className="flex items-start gap-4">
+                <div className="p-3 rounded-full bg-primary/10">
+                  <Code className="h-6 w-6 text-primary" />
+                </div>
+                <div className="text-left">
+                  <h4 className="font-semibold text-lg"> Web Development</h4>
+                  <p className="text-muted-foreground">
+                    Developing scalable, responsive web solutions using modern
+                    front-end and back-end frameworks.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="gradient-border p-6 card-hover">
+              <div className="flex items-start gap-4">
+                <div className="p-3 rounded-full bg-primary/10">
+                  <Phone className="h-6 w-6 text-primary" />
+                </div>
+                <div className="text-left">
+                  <h4 className="font-semibold text-lg">Mobile Development</h4>
+                  <p className="text-muted-foreground">
+                    Creating user-friendly mobile UIs and seamless
+                    cross-platform experiences.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="gradient-border p-6 card-hover">
+              <div className="flex items-start gap-4">
+                <div className="p-3 rounded-full bg-primary/10">
+                  <Briefcase className="h-6 w-6 text-primary" />
+                </div>
+
+                <div className="text-left">
+                  <h4 className="font-semibold text-lg">Project Management</h4>
+                  <p className="text-muted-foreground">
+                    Leading projects from conception to completion with agile
+                    methodologies.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
